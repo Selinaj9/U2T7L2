d769f020cb9d4c8815742bc165e89577b82ca2d0
bths <sample@bths.edu>
2025-11-26 10:48:07 -0500

    Factorial

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("What number do you want to halve to one? ");
+        System.out.println("What number do you want to factorial? ");
+        System.out.print("Enter a number between 0 and 170: ");
         int userNumber = scanner.nextInt();
-        int num = userNumber;
-        int timesHalf = 0;
-        if (userNumber == 0) {
-            System.out.println("It takes 0 halvings to get from 0 to 1");
-        } else {
-            while (num != 1) {
-                num /= 2;
-                System.out.println(num);
-                timesHalf++;
-            }
-            System.out.println("It takes " + timesHalf + " halvings to get from " + userNumber + " to 1.");
+
+
+        while ( userNumber < 0 || userNumber > 170 ) {
+            System.out.print("No! Between 0 and 170: ");
+            userNumber = scanner.nextInt();
         }
+
+
+        double i = 1;
+        double num = i;
+        while (i < userNumber) {
+            num *= i + 1;
+            i ++;
+        }
+        System.out.println(userNumber + "! is " + num);
     }
 }
